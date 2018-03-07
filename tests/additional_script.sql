fa68a239db1a8a3e7524c9d84adbf3a94fd68f47
Tobias Fischer <tobias.fischer@4fb.de>
2018-03-07 18:13:00 +0100

    updated test data

--- tests/additional_script.sql
+++ tests/additional_script.sql
@@ -1,1 +1,1 @@
-INSERT INTO `mysql_prepare_db`.`test_table` VALUES ('color_10','cyan',1),('color_20','ocre',0),('color_30','vanilla',1),('color_40','bordeaux',0);
+INSERT INTO `mysql_prepare_db`.`test_table` VALUES (5,'color_10','cyan',1),(6,'color_20','ocre',0),(7,'color_30','vanilla',1),(8,'color_40','bordeaux',0);
